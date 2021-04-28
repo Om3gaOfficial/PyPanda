4673308842b827eb87e2f972906a73863aaef74c
Om3ga | Chris <ckindermann.technik@gmail.com>
2021-04-28 21:33:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     name="PyPanda",
     version="0.0.1",
     author="Om3gaOfficial",
-    description="A python-libery for easier implementation of the 'Bitpanda' and 'Bitpanda-Pro' API in your projects",
+    description="PyPanda is an easy to use Python 3 based library providing simple and easy access to the API of Bitpanda and Bitpanda Pro.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Om3gaOfficial/PyPanda",
