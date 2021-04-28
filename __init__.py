85deedfc002a15620da8357c77780d3ece68212b
Om3ga | Chris <ckindermann.technik@gmail.com>
2021-04-28 21:21:18 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
 #Coded by Om3gaOfficial
-from PyPanda.main import BitPandaClass, BitPandaProClass  #Importing Classes from main file
+from PyPanda.pypanda import BitPandaClass, BitPandaProClass  #Importing Classes from main file
